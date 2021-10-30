ef447179c0eaa071ced84138743b4f2c0eb0eb9f
Igor Ryabchuk <riabtchuk@gmail.com>
2021-10-30 20:21:03 +0100

    Refactor BuyOneGetOneFreeDiscountCalculator

--- src/main/java/kata/supermarket/discountcalculators/BuyOneGetOneFreeDiscountCalculator.java
+++ src/main/java/kata/supermarket/discountcalculators/BuyOneGetOneFreeDiscountCalculator.java
@@ -12,7 +12,6 @@ import static java.util.stream.Collectors.reducing;
 
 public class BuyOneGetOneFreeDiscountCalculator implements DiscountCalculator{
 
-
     public BigDecimal calculateDiscount(List<Item> basket) {
         BigDecimal discountAmount = BigDecimal.ZERO.setScale(2);
 
@@ -20,15 +19,15 @@ public class BuyOneGetOneFreeDiscountCalculator implements DiscountCalculator{
             return discountAmount;
         }
 
-        Map<String, Integer> basketStats = getBasketStats(basket);
+        Map<String, Integer> statisticsForThisBasket = getBasketStats(basket);
         Map<String, BigDecimal> priceTableForThisBasket = new HashMap<>();
 
-        for (Item item : basket) {
-            if (!priceTableForThisBasket.containsKey(item.id())) {
-                priceTableForThisBasket.put(item.id(), item.price());
-            }
-        }
+        populatePriceBasket(basket, priceTableForThisBasket);
 
+        return getDiscountAmount(discountAmount, statisticsForThisBasket, priceTableForThisBasket);
+    }
+
+    private BigDecimal getDiscountAmount(BigDecimal discountAmount, Map<String, Integer> basketStats, Map<String, BigDecimal> priceTableForThisBasket) {
         for (Map.Entry<String, BigDecimal> entry : priceTableForThisBasket.entrySet()) {
             if (basketStats.containsKey(entry.getKey())) {
                 int howManyTimesToApplyDiscount =
@@ -38,8 +37,15 @@ public class BuyOneGetOneFreeDiscountCalculator implements DiscountCalculator{
                 discountAmount = discountAmount.add(amountOfDiscountForThisItem);
             }
         }
-
         return discountAmount;
+    }
+
+    private void populatePriceBasket(List<Item> basket, Map<String, BigDecimal> priceTableForThisBasket) {
+        for (Item item : basket) {
+            if (!priceTableForThisBasket.containsKey(item.id())) {
+                priceTableForThisBasket.put(item.id(), item.price());
+            }
+        }
     }
 
     private Map<String, Integer> getBasketStats(List<Item> basket) {
